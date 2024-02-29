3c1ac6a16512a1083521189b93777dc77352fcbd
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2024-02-29 11:24:34 -0800

    Started Java Script

---
+++ 36-Java Script/script.js
@@ -0,0 +1,66 @@
+
+
+// alert  is a opup  window that shows up in the browser
+// it's used to show information to users.
+alert("Wellcome to JAVA Script!");
+window.alert("Thanks for your input!");
+
+// var is use to decalar a Variable
+// Words in  double quotes are strings, and words in single quotes are not.
+var name = "Abrar";
+// if we need to change variable name we write 
+// name = "Ace"
+//  Now the console  will print Ace instead of Abrar
+
+var nationality;
+nationality = "U.S.";
+
+var floogle = "Mark";
+
+var lessonAuthor = "Jon";
+var guyWhoKeepsSayingHisOwnName = "Harry";
+var x = "Jonny";
+// JavaScript allow us to type in  whatever we want into variables
+
+// JavaScript allow us to type in two ways all smaii letters
+// like thsi (  hellomynameisharryaandilikecars    )
+// OR
+// camal Cases like thsi (  helloMyNameIsHarryaAndILikeCars    )
+
+
+// comments start with two slashes //
+/* this is also a comment */
+/* In JavaScript you can use comments like this one.
+   Comments start with two slash characters (/),  */
+
+// You can also make multiline comments using these syntaxes:
+/**/
+/* This is a multiline comment */
+/* It goes on until another  is found */
+
+var lastName = "_Smith_";
+var cityOfOrigin = "_New Orleans_";
+var aussieGreeting = "_g'Day_";
+
+
+var thanx = "Thank  your  Jaan Nii!";
+alert(thanx);
+
+
+// Variables for Numbers
+var worth = 100;
+var weight = 150;
+// WE Can use mathematical expressions  inside var statements, just like in any other place in JavaScript. For example:
+
+
+var originalNum = 23;
+var newNum = originalNum + 7;
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+
+// If you enclose a number in quotation marks and add 7...
+var originalNum = "23";
+var newNum = originalNum + 7;
+// if we write  the code above, it will not work because JavaScript thinks that "originalNum" is a string of text rather than a number. To fix this problem
