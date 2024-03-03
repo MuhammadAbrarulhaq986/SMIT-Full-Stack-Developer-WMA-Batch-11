612517526fbef9babcb9584fdc94b5759efd6a27
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2024-03-02 16:53:04 -0800

    JS Task Alert and Variables for Strings

--- 37-Java-Script-Tasks/script.js
+++ 37-Java-Script-Tasks/script.js
@@ -1,2 +1,38 @@
-alert( "Hello java Script");
- console.error( "This is an error message" );
+//  Alert First task
+// Error Message if user enter non-numeric value in input box
+//  console.log("This is a log message")
+// ; console.log("This is a log message");
+alert("Error! Plesae Enter a  valid number.");
+console.log("This is a Error message");
+// he log() method writes (logs) a message to the console.
+// The log() method is useful for testing purposes.
+
+
+//  Alert Second task
+alert("Wellcome to JS Land..\n Happy Coding! ");
+console.log("This is  a Welcome message");
+// By Using \n we can line  break, it's used to insert a new line of text into the document
+//  or a text area
+
+//  Alert Third task
+alert("Welcome  to JS Land");
+console.log("Welcome  to JS Land");
+
+//  Alert Fifth task
+alert("Hello... I'can run JS through my Web Browser's Console");
+console.log("Hello... I'can run JS through my Web Browser's Console");
+// *******************************************************************************************************************
+// TASK VARIABLES FOR STRINGS  
+var firstName = "Edward ";
+var lastName = " Elric";
+var fullname = firstName + lastName;
+alert(fullname);
+console.log(fullname);
+
+var age = " Age 19";
+alert(age);
+console.log(age);
+
+
+
+// *******************************************************************************************************************
