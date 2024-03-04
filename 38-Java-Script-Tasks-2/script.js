3122db2ed6edce722809ae62a7c4d8074e00682b
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2024-03-03 22:58:10 -0800

    JS TASK VARIABLE NAMES: LEGAL & ILLEGAL COMPLETE

---
+++ 38-Java-Script-Tasks-2/script.js
@@ -0,0 +1,17 @@
+// ************ VARIABLE NAMES: LEGAL & ILLEGAL  **********************************************************************************
+var displayOne = "Variable names can only contain,  numbers, $ and _ . For example: $my_1stVariable <br>";
+document.write(displayOne);
+console.log(displayOne);
+
+var displayTwo = "Variable must begin with a letter, $ or _. For example: $name, _name or name <br>";
+document.write(displayTwo);
+console.log(displayTwo);
+
+var displayThree = "Variable names are case sensitive <br>";
+document.write(displayThree);
+console.log(displayThree);
+
+var displayfour = "Variable names should not be JS keywords <br>";
+document.write(displayfour);
+console.log(displayfour);
+// ************ VARIABLE NAMES: LEGAL & ILLEGAL COMPLETE **********************************************************************************
