6742b049582ce53f527674f77a69d002431c5caa
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2024-03-04 00:15:56 -0800

    JS Task

---
+++ 39-Java-Script-Tasks-3/script.js
@@ -0,0 +1,51 @@
+//this statement will print the string "The sum of 3 and 5 is" followed by the result of adding 3 and 5
+var Plus = " The Sum of 5 and 10 is " + (5 + 15);
+document.write(Plus + " <br>");
+console.log(Plus);
+
+var Minus = " The subtraction of 15 and 10 is " + (15 - 10);
+document.write(Minus + " <br>");
+console.log(Minus);
+
+var Multiply = " The multiplication of 5 and 100 is " + (5 * 100);
+document.write(Multiply + " <br>");
+console.log(Multiply);
+
+var Division = " The subtraction of 20 and 10 is " + (20 / 10);
+document.write(Division + " <br>");
+console.log(Division);
+// ***********************************************************************************************************************
+var First = "  Value after variable declaration is: ??. ";
+document.write(First + " <br>");
+console.log(First);
+
+var Second = "  Initial value: 5 ";
+document.write(Second + " <br>");
+console.log(Second);
+
+// Increment
+var countOne = 5;
+document.write(countOne + " - Initial value <br>");
+document.write("Incrementing the value to: " + (countOne++) + " <br>");
+document.write("Value after incrementing: " + countOne + " <br>");
+
+var countOne = 5;
+document.write(countOne + " - Initial value <br>");
+document.write("Incrementing the value to: " + (++countOne) + " <br>");
+document.write("Value after incrementing: " + countOne + " <br>");
+
+//  Decrement
+var countTwo = 5;
+document.write(countTwo + " - Initial value <br>");
+document.write("Decrementing the value to: " + (countTwo--) + " <br>");
+document.write("Value after Decrementing: " + countTwo + " <br>");
+var countTwo = 5;
+
+document.write(countTwo + " - Initial value <br>");
+document.write("Decrementing the value to: " + (--countTwo) + " <br>");
+document.write("Value after Decrementing: " + countTwo + " <br>");
+
+// Remainder
+var countThree = 89;
+document.write(countThree + " - Initial value <br>");
+document.write("Remainder after dividing by 5: " + (countThree % 2) + " <br>");
