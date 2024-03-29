4c4524f79a617d88ca554d0ef7d69f2b885723ac
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2024-03-29 12:23:00 -0700

    4 CHAPTER COMPLETED

---
+++ 01-Java-Script-Book-Nots-chapter-1-to-10/script.js
@@ -0,0 +1,73 @@
+// ******************** ALERT *****************************************************************************************************************************************************************************************************************
+var ThisIsAlert = ("This is  how  Alert!  Works ");
+
+alert("Welcome\n to\nJavaScript \nBasic Nots ");
+
+alert("We are here  for your help");
+document.write("<h1>Alert Message is used to display message in your Browser </h1>")
+
+var message = "Welcome " + "to " + "JavaScript " + "Basic Nots";
+alert(message);
+
+
+window.alert("This is a window Alert, it will show up when you click on OK button.");
+document.write("<h1>This is a window Alert, it will show up when you click on OK button.</h1>");
+
+document.write("<h1>" + ThisIsAlert + "</h1>");
+// *************************************************************************************************************************************************************************************************************************************
+
+// ************** Variables for Strings ***********************************************************************************************************************************************************************************************************************
+
+var Variables = "<h2>Variables for String</h2>";
+document.write(Variables);
+
+var Name = "<h2>Mark</h2>";
+Name = "<h2>Mark Ace</h2>";
+document.write(Name);
+
+var Age = 35;
+document.write("<h2>My name is " + Name + " I am  " + Age + " years old. </h2>");
+
+var Color = "<h2>Color of my Shirt:<b>Blue</b></h2>";
+document.write(Color);
+
+var thanx = "<h2>Thanks for your input! </h2> "
+document.write(thanx);
+
+// ************** Variables for Numbers ***********************************************************************************************************************************************************************************************************************
+
+
+var weightOne = 150;
+document.write("<h2>Normal " + weightOne + " </h2>");
+
+var weightTwo = 100;
+weightTwo + 30;
+document.write("<h2>Puls + 30 to Weight " + weightTwo + " </h2>");
+
+var weightThree = 50;
+weightThree - 20;
+document.write("<h2>Minus - 20 to Weight " + weightThree + " </h2>");
+
+var originalWeight = 100;
+var overWeight = 200;
+
+originalWeight = overWeight + 10;
+document.write("<h2>Original Weight was " + originalWeight + ", but now its " + overWeight + ". So Original Weight plus 10");
+
+var originalStringNum = "23";
+var originalWeight = originalStringNum + 7;
+document.write(" <h2> When you Write a number in Coma it becomes a string " + originalStringNum + " As you can see " + originalWeight + " it is showing 237 </h2> ");
+
+
+// ************** Variable Names Legal and Illegal ***********************************************************************************************************************************************************************************************************************
+// Here are some examples of legal variable names:
+// firstName
+// lastName
+// $name
+// _age
+// numberOfStudents
+// numberOfApples
+// $num1
+// _num_2
+
+
--- 01-Java-Script-Book-Nots/script.js
+++
@@ -1,7 +0,0 @@
-alert("Welcome\n to\nJavaScript \nBasic Nots ");
-
-alert("We are here  for your help");
-document.write("<h1>Alert Message is used to display message in your Browser </h1>")
-
-var message = "Welcome " + "to " + "JavaScript " + "Basic Nots";
-alert(message);
