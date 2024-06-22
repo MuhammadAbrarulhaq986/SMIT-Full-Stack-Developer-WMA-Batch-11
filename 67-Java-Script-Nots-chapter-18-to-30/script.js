b81e6bc9b3c51e27a7e468646ee96abae71ee92c
Muhammad Abrar ul haq <133530676+TkdAbrar@users.noreply.github.com>
2024-06-22 23:55:19 +0500

    26 chapter complete from the JS book

--- 67-Java-Script-Nots-chapter-18-to-30/script.js
+++ 67-Java-Script-Nots-chapter-18-to-30/script.js
@@ -382,36 +382,234 @@
 
 
 
-//* Get the month from the user
-var month = prompt("Enter a month");
+// //* Get the month from the user
+// var month = prompt("Enter a month");
 
-// *Abbreviate the month if it's more than 3 characters long
-var monthAbbrev = month.substring(0, 3);
+// // *Abbreviate the month if it's more than 3 characters long
+// var monthAbbrev = month.substring(0, 3);
 
-//* Get the text from the user
-var str = prompt("Enter some text");
+// //* Get the text from the user
+// var str = prompt("Enter some text");
 
-//* Check for double spaces in the text
-for (var i = 0; i < str.length; i++) {
-    if (str.charAt(i) === " " && str.charAt(i + 1) === " ") {
-        alert("Double spaces found!");
-        break;
-    }
-}
+// //* Check for double spaces in the text
+// for (var i = 0; i < str.length; i++) {
+//     if (str.charAt(i) === " " && str.charAt(i + 1) === " ") {
+//         alert("Double spaces found!");
+//         break;
+//     }
+// }
 
-console.log(monthAbbrev);
-console.log(str);
+// console.log(monthAbbrev);
+// console.log(str);
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// //* Assign the paragraph to the variable text
+// let text =
+//     "The New Yorker magazine doesn't allow the phrase 'World War II.' They say it should be 'the Second World War'. So let's search the following sentence for the banned characters and replace them with the phrase that the New Yorker prefers. It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his. ";
+
+// //* Loop through the string character by character
+// for (var i = 0; i < text.length; i++) {
+//     //* Check if the 12-character sequence starting at index i is "World War II"
+//     if (text.slice(i, i + 12) === "World War II") {
+//       //* If it is, replace it with "the Second World War"
+//       text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+//     }
+//   }
+
+//   // //* Output the updated text
+// document.write(text);
+
+// //* Assign the paragraph to the variable text
+// let text =
+//   "The New Yorker magazine doesn't allow the phrase 'World War II.' They say it should be 'the Second World War'. So let's search the following sentence for the banned characters and replace them with the phrase that the New Yorker prefers. It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his. ";
+
+// //* Use the indexOf method to find the index of the first occurrence of "World War II" in the text
+// let firstChar = text.indexOf("World War II");
+
+// //* If the phrase "World War II" is found, replace it with "the Second World War"
+// if (firstChar !== -1) {
+//   //* Slice the text into three parts: before the banned phrase, the banned phrase itself, and after the banned phrase
+//   text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
+// }
+
+// //* Output the updated text
+// document.write(text);
+// ********************************************************************************
+// //* The indexOf method returns the index of the first occurrence of a specified value in a string.
+
+// //* Example:
+// var text = "Hello World, Hello Universe!";
+// var searchValue = "Hello";
+
+// //* Use the indexOf method to find the index of the first occurrence of "Hello" in the text
+// var index = text.indexOf(searchValue);
+
+// //* Output the result
+// console.log("The index of \"" + searchValue + "\" is " + index); //* Output: The index of "Hello" is 0
+
+// //* If the value is not found, indexOf returns -1
+// var notFoundValue = "Goodbye";
+// var notFoundIndex = text.indexOf(notFoundValue);
+
+// console.log("The index of \"" + notFoundValue + "\" is " + notFoundIndex); //* Output: The index of "Goodbye" is -1
+
+
+// ********************************************************************************
+
+// var text = "Hello World, Hello Universe!";
+// var searchValue = "Hello";
+
+// //* Use the lastIndexOf method to find the index of the last occurrence of "Hello" in the text
+// var lastIndex = text.lastIndexOf(searchValue);
+
+// //* Output the result
+// console.log("The last index of \"" + searchValue + "\" is " + lastIndex); //* Output: The last index of "Hello" is 13
+
+// //* If the value is not found, lastIndexOf returns -1
+// var notFoundValue = "Goodbye";
+// var notFoundIndex = text.lastIndexOf(notFoundValue);
+
+// console.log("The last index of \"" + notFoundValue + "\" is " + notFoundIndex);
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// var firstName = "Abrar";
+// var lastName = "ul haq";
+// console.log(firstName , lastName);
+
+// var firstChar = firstName.slice(0, 1);
+// console.log(firstChar);
+
+// var firstChar = firstName.charAt(0)
+// console.log(firstChar);
+
+
+// var firstName = "Abrar";
+// var lastName = "ul haq";
+// console.log(firstName , lastName);
+
+// //* Define a string variable
+// var name = "Abrar ul haq";
+
+// //* Get the last character of the string
+// var lastChar = name.charAt(name.length - 1);
+
+// //* Print the result to the console
+// console.log("The last character of the string is:", lastChar);
+
+
+// //* Define the text to search for exclamation points
+// const text = "Hello! World!";
+
+// //* Loop through each character in the text
+// for (let i = 0; i < text.length; i++) {
+//   //* Check if the current character is an exclamation point
+//   if (text[i] === "!") {
+//     //* If an exclamation point is found, alert the user
+//     alert("Exclamation point found at index " + i);
+//     //* Stop searching the rest of the text
+//     break;
+//   }
+// }
+
+
+// //* Define the text to search for exclamation points
+// const text = "Hello! World!";
+
+// //* Loop through each character in the text
+// for (let i = 0; i < text.length; i++) {
+//   //* Check if the current character is an exclamation point
+//   if (text[i] === "!") {
+//     //* If an exclamation point is found, alert the user
+//     alert("Exclamation point found at index " + i);
+//   }
+// }
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+// let text =
+//     "The New Yorker magazine doesn't allow the phrase 'World War II.' They say it should be 'the Second World War'. So let's search the following sentence for the banned characters and replace them with 'World War II.'the phrase that the New Yorker prefers. It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his. ";
+
+// for (var i = 0; i < text.length; i++) {
+//     if (text.slice(i, i + 12) === "World War II") {
+//         text = text.slice(0, 1) + "the Second World War" + text.slice(i + 12);
+//     }
+// }
+// document.write(text)
+
+
+// let text =
+//     "The New Yorker magazine doesn't allow the phrase 'World War II.' They say it should be 'the Second World War'. So let's search the following sentence for the banned characters and replace them with 'World War II.'the phrase that the New Yorker prefers. It is startling to think that, even in the darkest depths of 'World War II.' J. R. R. Tolkien was writing the trilogy, 'World War II.' which contains, with the weird applicability available only to poetry and myth, 'World War II.' the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his. ";
+
+//     var firstChar = text.indexOf("World War II");
+//  if (firstChar !== -1) {
+//  text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
+//  }
+
+// document.write(text);
+
+// var text =
+//     "The New Yorker magazine doesn't allow the phrase 'World War II.' They say it should be 'the Second World War'. So let's search the following sentence for the banned characters and replace them with the phrase that the New Yorker prefers. It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his. ";
 
 
 
+// text = text.replace("World War II", "the Second World War")
+
+// document.write(text);
+
+// var text =
+//     "The New Yorker magazine doesn't allow the phrase 'World War II.' They say it should be 'the Second World War'. So let's search the following sentence for the banned characters and replace them with the phrase that the New Yorker prefers. It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy World War II, which contains, with the weird applicability available only to poetry and myth World War II, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his. ";
 
 
 
+// var newText = text.replace(/World War II/g, "the Second World War");
+
+// document.write(newText);
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//* Example of using Math.round() to round a number to the nearest integer
+
+//* Define a decimal number
+// let decimalNumber = 4.7;
+
+// //* Use Math.round() to round the number to the nearest integer
+// let roundedNumber = Math.round(decimalNumber);
+
+// //* Log the result to the console
+// console.log("The rounded number is: " + roundedNumber);
+//* Output: The rounded number is: 5
+// roundedNumber = Math.round(roundedNumber);
+
+// console.log(roundedNumber);
+
+
+// //* Another example with a negative number
+// let negativeDecimalNumber = -3.4;
+
+// //* Use Math.round() to round the number to the nearest integer
+// let roundedNegativeNumber = Math.round(negativeDecimalNumber);
+
+// //* Log the result to the console
+// console.log("The rounded negative number is: " + roundedNegativeNumber); //* Output: The rounded negative number is: -3
 
 
 
+// //*  Declare a variable to store a decimal number
+// let decimalNumber = .023415789;
+
+// //*  Use Math.ceil() to round the decimal number up to the nearest integer
+// let roundedNumber = Math.ceil(decimalNumber);
+
+// //* Log the result to the console
+// console.log("The rounded number is: " + roundedNumber);
 
 
+// //* Declare a variable to store a decimal number close to 1
+// let decimalNumber = 0.99999999;
 
+// //* Use the Math.floor() function to round the decimal number down to the nearest whole number
+// let roundedNumber = Math.floor(decimalNumber);
 
-
+// //* Log the result to the console
+// console.log("The rounded number is: " + roundedNumber);
