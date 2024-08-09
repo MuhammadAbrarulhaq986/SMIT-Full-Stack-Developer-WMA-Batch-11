88164356837b244ccc70712e1cc7d3ba7274af1b
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2024-08-09 19:19:41 +0500

    Firebase Crud App done

--- 87-firebase-crud/script.js
+++ 87-firebase-crud/script.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
+import { getDatabase, set, ref, get, remove, update } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,7 +19,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-const add_data = document.getElementById("add_data");
+const add_date = document.getElementById("add_date");
 const notification = document.getElementById("notification");
 
 function AddStudents() {
@@ -34,9 +34,87 @@ function AddStudents() {
         rollnumber: rollnumber,
     })
     notification.innerText = "Added succesfully";
-    document.getElementById('name').value;
-    document.getElementById('email').value;
-    document.getElementById('rollnumber').value;
-}
+    document.getElementById('name').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('rollnumber').value = "";
 
-add_data.addEventListener('click', AddStudents)
+    ReadData();
+
+}
+add_date.addEventListener('click', AddStudents)
+
+
+//* READ DATA 
+function ReadData() {
+    const userRef = ref(db, "students/");
+
+    get(userRef).then((snapshort => {
+        // console.log(snapshort.val());//* TO CHECK IN CONSOLE
+
+        const data = snapshort.val();
+        const table = document.querySelector('table');
+        let html = '';
+
+        for (const key in data) {
+            const { name, email, rollnumber } = data[key];
+            // console.log(name); //* TO CHECK IN CONSOLE
+            html += `<tr>       
+            <td>${name}</td>
+            <td>${email}</td>
+            <td>${rollnumber}</td>
+            <td><button class="del" onclick="deleteDate('${rollnumber}')">Delete</button></td>
+            <td><button class="up" onclick="updateDate('${rollnumber}')">Update</button></td>
+            </tr>`;
+        }
+        table.innerHTML = html;
+    }))
+}
+ReadData();
+//* deleteDate IS BECOME A GLOBAL SCOPE
+window.deleteDate = function (rollnumber) {
+    // alert(rollnumber); //* TO CHECK IN alert
+    const userRef = ref(db, `students/${rollnumber}`);
+    remove(userRef);
+    notification.innerText = "Deleted succesfully";
+
+    ReadData();
+}
+//* updateDate IS BECOME A GLOBAL SCOPE
+window.updateDate = function (rollnumber) {
+    // alert(rollnumber); //* TO CHECK IN alert
+    const userRef = ref(db, `students/${rollnumber}`);
+
+    get(userRef).then((item) => {
+        // console.log(item.val()); //* TO CHECK IN CONSOLE
+        document.getElementById('name').value = item.val().name;
+        document.getElementById('email').value = item.val().email;
+        document.getElementById('rollnumber').value = item.val().rollnumber;
+    });
+
+    document.querySelector('.update_Data').classList.add('show')
+
+    const update_btn = document.querySelector('#update_data');
+
+    update_btn.addEventListener('click', () => {
+        // alert(''); //* TO CHECK IN alert
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const rollnumber = document.getElementById('rollnumber').value;
+
+        update(ref(db), {
+            [`students/${rollnumber}/name`]: name,
+            [`students/${rollnumber}/email`]: email,
+            [`students/${rollnumber}/rollnumber`]: rollnumber,
+        }).then(() => {
+            notification.innerText = "data Updated";
+            document.querySelector('.update_Data').classList.remove('show');
+
+            document.getElementById('name').value = "";
+            document.getElementById('email').value = "";
+            document.getElementById('rollnumber').value = "";
+
+
+            ReadData();
+        })
+    })
+}
